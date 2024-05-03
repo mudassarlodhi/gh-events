44567cfc31c33b0b1cd3af39eeac0b9ad6992397
mudassarlodhi <mudasserlodhi@yahoo.com>
2024-05-04 02:20:47 +0500

    class name chanhed for help area

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -26,7 +26,7 @@ const HELP_ITEMS = [
 
 function HelpArea() {
   return (
-    <section data-testid="help-area" id="help-area" className="help-areaaa">
+    <section data-testid="help-area" id="help-area" className="help-area">
       {HELP_ITEMS?.map((item) => (
         <HelpBox key={item.id} title={item.title} text={item.text} />
       ))}
